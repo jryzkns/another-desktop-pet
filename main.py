62f1010b553125cecbee2a16a04e4a8897843583
jryzkns <jryzkns@gmail.com>
2021-01-07 23:53:32 -0800

    update select and drag mechanism; add boundcheck

--- main.py
+++ main.py
@@ -28,11 +28,14 @@ class Pet(tk.Tk):
         self.dx, self.dy = 0, 0
         
         # BINDINGS; COULD BIND EXTRA THINGS DOWN THE LINE
-        # self.bind("<Button-1>", self.drag_event)
+        self.bind("<ButtonPress-1>", self.start_move)
+        self.bind("<ButtonPress-3>", self.handle_rclick)
+        self.bind("<B1-Motion>", self.do_move)
 
         # PET ATTRIBUTES
         self.x, self.y = 100, 100
         self.state = STATE_S2I  # initial state is waking up
+        self.movespeed = 3
         self.event_num = ri(1,3)
 
         get_frame_buffer = lambda path, n_frames : \
@@ -57,7 +60,6 @@ class Pet(tk.Tk):
             STATE_WL    : (1, 10),
             STATE_WR    : (1, 10)}
 
-        # TODO: what does this do?
         self.transition_timer = {
             STATE_IDLE  : 400,
             STATE_I2S   : 100,
@@ -66,29 +68,41 @@ class Pet(tk.Tk):
             STATE_WL    : 100,
             STATE_WR    : 100}
 
-        self.bind("<ButtonPress-1>", self.start_move)
-        self.bind("<B1-Motion>", self.do_move)
+    def validate_xy(self):
+        self.x, self.y = \
+            0                     if self.x < 0 else \
+            self.screen_w - 100   if self.x + 100 > self.screen_w else \
+            self.x, \
+            0                     if self.y < 0 else \
+            self.screen_h - 100   if self.y + 100 > self.screen_h else \
+            self.y
 
-    def start_move(self, e):
-        self.dx, self.dy = e.x, e.y
+    def handle_rclick(self, event): print("bye!"); self.quit()
+
+    def start_move(self, e): self.dx, self.dy = e.x, e.y
 
     def do_move(self, e):
+
         dx, dy = e.x - self.dx, e.y - self.dy
         self.x, self.y = self.winfo_x() + dx, self.winfo_y() + dy
+        self.validate_xy()
         self.geometry(f"100x100+{self.x}+{self.y}")
 
     def update(self):
-        self.anim_idx = (self.anim_idx+1) % len(self.anims[self.state])
+
+        self.anim_idx = (self.anim_idx + 1) % len(self.anims[self.state])
         self.event_num = ri(*self.first_last[self.state])
 
-        if self.state == STATE_WL:  self.x -= 3
-        if self.state == STATE_WR:  self.x -= -3
+        if self.state == STATE_WL: self.x -= self.movespeed
+        if self.state == STATE_WR: self.x += self.movespeed
 
+        self.validate_xy()
         self.geometry(f'100x100+{self.x}+{self.y}')
         self.label.configure(image=self.anims[self.state][self.anim_idx])
         self.after(1, self.event)
 
     def event(self):
+
         if self.event_num in idle_num:     self.state = STATE_IDLE
         elif self.event_num == 5:          self.state = STATE_I2S
         elif self.event_num in walk_left:  self.state = STATE_WL
